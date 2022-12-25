14e08b3c337829944a4873c6ce11adb5e2601f8d
Vieira <weverson.f.vieira@accenture.com>
2022-12-25 12:53:38 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/weverson/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.weverson.course.repositories;
+
+import com.weverson.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
