f76e0916d9fe39688606a5ac3c39ce4a40a4d8c5
Vieira <weverson.f.vieira@accenture.com>
2022-12-25 18:48:25 -0300

    OrderStatus enum

--- src/main/java/com/weverson/course/entities/Order.java
+++ src/main/java/com/weverson/course/entities/Order.java
@@ -1,6 +1,7 @@
 package com.weverson.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.weverson.course.entities.enuns.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -21,13 +22,16 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private User client;
 
+    private Integer orderStatus;
+
     public Order() {
 
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -53,6 +57,17 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
+
     }
 
     @Override
